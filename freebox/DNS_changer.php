bd1e67aa9b745129fa733c69427c358d0270d3e1
alphayax <alphayax@gmail.com>
2016-02-27 20:46:41 +0100

    - Add check - Use args to launch script

--- freebox/DNS_changer.php
+++ freebox/DNS_changer.php
@@ -1,7 +1,9 @@
 <?php
 namespace alphayax\freebox;
+use alphayax\freebox\api\v3 as FreeboxAPI;
 use alphayax\opennic;
 use alphayax\google;
+use alphayax\utils\cli\IO;
 
 /**
  * Class DNS_changer
@@ -11,32 +13,90 @@ class DNS_changer {
 
     const APP_ID        = 'com.alphayax.freebox.dns_changer';
     const APP_NAME      = 'Freebox DNS changer';
-    const APP_VERSION   = '1.0.2';
+    const APP_VERSION   = '1.0.3';
 
     const FREEBOX_HOST  = 'mafreebox.freebox.fr';
 
+    /** @var utils\Application Freebox App */
+    protected $app;
+
+    /** @var bool Verbose Mode */
+    protected $isVerbose;
+
     /**
      * DNS_changer constructor.
+     * @param bool $verbose
      */
-    public function __construct(){
-
+    public function __construct( $verbose = false){
         /// Define our application
-        $App = new utils\Application( self::APP_ID, self::APP_NAME, self::APP_VERSION);
-        $App->authorize();
-        $App->openSession();
+        $this->app = new utils\Application( self::APP_ID, self::APP_NAME, self::APP_VERSION);
+        $this->app->authorize();
+        $this->app->openSession();
+        /// Saving others parameters
+        $this->isVerbose = $verbose;
+    }
 
+    /**
+     * Check current DNS status
+     * @return bool
+     */
+    public function checkDNS(){
+        if( $this->isVerbose){
+            IO::stdout( 'Checking DNS Servers...', 0, true, IO::COLOR_MAGENTA);
+        }
+        $isRespondingDNS = true;
+        $Config = new FreeboxAPI\services\config\DHCP( $this->app);
+        $currentConfig = $Config->get_current_configuration();
+        $a = $currentConfig->dns;
+        foreach( $a as $dnsserver){
+            $output = null;
+            $exit   = null;
+            if( empty( $dnsserver)){
+                if( $this->isVerbose){
+                    IO::stdout( "Server not defined. Skip", 1, true, IO::COLOR_YELLOW);
+                }
+                continue;
+            }
+            exec( 'nslookup -timeout=1 t411.io '. $dnsserver, $output, $exit);
+            if( empty( $exit)){
+                if( $this->isVerbose){
+                    IO::stdout( "Server $dnsserver is OK", 1, true, IO::COLOR_GREEN);
+                }
+            } else {
+                if( $this->isVerbose){
+                    IO::stdout( "Server $dnsserver is OK", 1, true, IO::COLOR_RED);
+                }
+                $isRespondingDNS = false;
+            }
+        }
+        return $isRespondingDNS && ! empty( $a);
+    }
+
+    /**
+     * Update DNS
+     * Get new DNS servers from openDNS and Google
+     */
+    public function updateDNS(){
+        if( $this->isVerbose){
+            IO::stdout( 'Updating DNS Servers...', 0, true, IO::COLOR_MAGENTA);
+        }
         /// Find new DNS servers
         $opennic_DNS_servers = opennic\DNS_server::get_nearest_servers();
         $google_DNS_servers  = google\DNS_server::get_nearest_servers();
         $new_DNS_servers = array_merge( $opennic_DNS_servers, $google_DNS_servers);
 
         /// Update Configuration
-        $Config = new api\v3\dhcp\DHCP( $App);
-        $prevConfig = $Config->get_current_configuration();
-        $newConfig  = $Config->set_attribute_configuration(['dns' => $new_DNS_servers]);
+        $Config = new FreeboxAPI\services\config\DHCP( $this->app);
+        $Config->set_attribute_configuration(['dns' => $new_DNS_servers]);
+        $newConfig = $Config->get_current_configuration();
 
-        /// Trace
-        var_dump( $prevConfig, $newConfig, $new_DNS_servers);
+        if( $this->isVerbose){
+            $a = $newConfig->dns;
+            IO::stdout( 'New DNS Servers :', 0, true, IO::COLOR_MAGENTA);
+            foreach( $a as $ServerDNS){
+                IO::stdout( "- ". ($ServerDNS ?: 'Undefined'), 1);
+            }
+        }
     }
 
 }
--- freebox_dns_changer.php
+++ freebox_dns_changer.php
@@ -1,11 +1,44 @@
 <?php
 namespace alphayax;
+use alphayax\utils\cli\GetOpt;
+use alphayax\utils\cli\IO;
 
 /**
  * Freebox DNS Auto updater
  * @author <alphayax@gmail.com>
  */
+
 require_once __DIR__ . '/vendor/autoload.php';
 
+$Args = new GetOpt();
+$Args->addShortOpt( 'v', 'Enable verbose mode');
+$Args->addLongOpt( 'verbose'    , 'Enable verbose mode');
+$Args->addLongOpt( 'checkdns'   , 'Check current DNS status');
+$Args->addLongOpt( 'udpatedns'  , 'Update DNS servers');
+$Args->parse();
+
+$isVerbose = $Args->hasOption( 'v') || $Args->hasOption( 'verbose');
+
 /// Launch app
-new freebox\DNS_changer();
+$App = new freebox\DNS_changer( $isVerbose);
+
+// Check status
+if( $Args->hasOption('checkdns')){
+    if( ! $App->checkDNS()){
+        if( $isVerbose){
+            IO::stdout( 'DNS seems to be corrupted. Forced update...', 0, true, IO::COLOR_MAGENTA);
+        }
+        $App->updateDNS();
+    }
+}
+
+// Force update
+if( $Args->hasOption('updatedns')){
+    $App->updateDNS();
+}
+
+// No args given
+if( ! $Args->hasOption('updatedns') && ! $Args->hasOption('checkdns')){
+    IO::stdout( 'Please use the --checkdns or --updatedns flag');
+}
+
